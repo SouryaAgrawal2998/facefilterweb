698b9a5a87ccedaa7e88fbbb8e91464b0d6f1262
SouryaAgrawal2998 <81215092+SouryaAgrawal2998@users.noreply.github.com>
2022-01-22 12:42:34 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
+noseX= 0;
+noseY= 0;
 function preload(){
-
+  mustache=loadImage("https://i.postimg.cc/3x3QzSGq/m.png");
 }
 function setup(){
  canvas= createCanvas(300, 300);
@@ -16,12 +18,16 @@ function gotPoses(results){
         console.log("face x="+results[0].pose.nose.x);
         console.log("face y="+results[0].pose.nose.y);
     }
+ noseX= results[0].pose.nose.x-20;
+ noseY= results[0].pose.nose.y-10;
+
 }
 function modelLoaded(){
   console.log("Model has been loaded");
 }
 function draw(){
     image(video,0,0,300,300);
+    image(mustache,noseX,noseY,50,50);
 }
 function take_snapshot(){
     save('myimg.png');
